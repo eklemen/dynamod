0ccec1261bb251359ec15f1b03bf901ebd3ee993
eklemen <ejklemen@gmail.com>
2021-08-17 15:09:40 -0400

    updates to package.json

--- cli.js
+++ cli.js
@@ -1,7 +1,12 @@
+#!/usr/bin/env node
 const fs = require('fs');
 const chalk = require('chalk');
 // const rimraf = require('rimraf');
 const moduleName = process.argv[2];
+if (!moduleName) {
+  console.error(chalk.red.bold('Must supply a name for this module. Ex: $ twig my-module'));
+  process.exit(1);
+}
 const {
   Context,
   scaffold,
