123fc2cb1a32a2318ad6109b49099d3cc6de7cdd
eklemen <ejklemen@gmail.com>
2021-08-17 14:38:14 -0400

    tests

---
+++ utils.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const chalk = require('chalk');
+const Handlebars = require('handlebars');
+
+const kebabCase = require('lodash.kebabcase');
+const capitalize = require('lodash.capitalize');
+const camelCase = require('lodash.camelcase');
+const startCase = require('lodash.startcase');
+const snakeCase = require('lodash.snakecase');
+
+function Context(modName) {
+  this.name = modName;
+  this.capital = snakeCase(this.name).toUpperCase();
+  this.kebab = kebabCase(this.name.toLowerCase());
+  this.pascal = startCase(this.name).replace(/ /g, '');
+  this.camel = camelCase(this.name)
+  this.moduleName = `${this.pascal}Module`;
+  this.serviceName = `${this.pascal}Service`;
+}
+
+const buildTemplate = (type, context) => {
+  const currentDir = process.cwd();
+  const template = fs.readFileSync(`templates/${type}.hbs`, { encoding: 'utf-8' });
+  const compiled = Handlebars.compile(template);
+  const output = compiled(context);
+  const fileName = `${context.kebab}.${type}.ts`;
+  fs.writeFileSync(`${currentDir}/${context.camel}/${fileName}`, output);
+  console.log(chalk.blue(`Created file: ${fileName}`));
+};
+
+const buildInterfaceTemplates = (context) => {
+  const currentDir = process.cwd();
+  // interfaces
+  let template = fs.readFileSync(`templates/interfaces.hbs`, { encoding: 'utf-8' });
+  let compiled = Handlebars.compile(template);
+  let output = compiled(context);
+  let fileName = `${context.kebab}-module.interfaces.ts`;
+  fs.writeFileSync(`${currentDir}/${context.camel}/interfaces/${fileName}`, output);
+  // barrel file
+  template = fs.readFileSync(`templates/interfacesIndex.hbs`, { encoding: 'utf-8' });
+  compiled = Handlebars.compile(template);
+  output = compiled(context);
+  fileName = 'index.ts';
+  fs.writeFileSync(`${currentDir}/${context.camel}/interfaces/${fileName}`, output);
+  console.log(chalk.blue(`Created file: ${fileName}`));
+};
+
+const scaffold = (context, filesToBuild) => {
+  const currentDir = process.cwd();
+  try {
+    fs.mkdirSync(`${currentDir}/${context.camel}`);
+    fs.mkdirSync(`${currentDir}/${context.camel}/interfaces`);
+    for (let type of filesToBuild) {
+      buildTemplate(type, context);
+      buildInterfaceTemplates(context);
+    }
+    console.log(
+      chalk.bold.greenBright(
+        `Success. Created package at ${currentDir}/${context.camel}`
+      ));
+  } catch (err) {
+    // rimraf(`${currentDir}/${context.camel}`);
+    console.log(
+      chalk.red.bold(`Error scaffolding module: ${err}`)
+    );
+  }
+}
+
+module.exports = {
+  Context,
+  buildTemplate,
+  buildInterfaceTemplates,
+  scaffold
+}
---
+++ utils.test.js
@@ -0,0 +1,118 @@
+const fs = require('fs');
+
+const {
+  Context,
+  buildTemplate,
+  buildInterfaceTemplates,
+  scaffold
+} = require('./utils');
+
+describe('Utils', () => {
+  describe('Context constructor', () => {
+    let context;
+    test('should format all lowercase one word', () => {
+      context = new Context('lowercase');
+      expect(context).toEqual({
+        camel: 'lowercase',
+        capital: 'LOWERCASE',
+        kebab: 'lowercase',
+        moduleName: 'LowercaseModule',
+        name: 'lowercase',
+        pascal: 'Lowercase',
+        serviceName: 'LowercaseService'
+      })
+    });
+    test('should format all lowercase two words', () => {
+      context = new Context('lower-two');
+      expect(context).toEqual({
+        camel: 'lowerTwo',
+        capital: 'LOWER_TWO',
+        kebab: 'lower-two',
+        moduleName: 'LowerTwoModule',
+        name: 'lower-two',
+        pascal: 'LowerTwo',
+        serviceName: 'LowerTwoService'
+      })
+    });
+    test('should format all mixed case one word', () => {
+      context = new Context('Test');
+      expect(context).toEqual({
+        camel: 'test',
+        capital: 'TEST',
+        kebab: 'test',
+        moduleName: 'TestModule',
+        name: 'Test',
+        pascal: 'Test',
+        serviceName: 'TestService'
+      })
+    });
+  });
+
+  describe('buildTemplate and buildInterfaceTemplates', () => {
+    let context;
+    beforeEach(() => {
+      jest.spyOn(fs, 'writeFileSync').mockReturnValue(null);
+      jest.spyOn(fs, 'readFileSync');
+      jest.spyOn(process, 'cwd').mockReturnValue('cwd');
+      context = {
+        camel: 'test',
+        capital: 'TEST',
+        kebab: 'test',
+        moduleName: 'TestModule',
+        name: 'test',
+        pascal: 'Test',
+        serviceName: 'TestService'
+      }
+    });
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+    test('build template should writeFileSync', () => {
+      buildTemplate('module', context)
+      expect(fs.readFileSync).toHaveBeenCalledWith('templates/module.hbs', {encoding: 'utf-8'});
+      expect(fs.writeFileSync.mock.calls[0][0]).toEqual('cwd/test/test.module.ts');
+    });
+
+    test('buildInterfaceTemplate should writeFileSync twice', () => {
+      buildInterfaceTemplates(context)
+      expect(fs.readFileSync).toHaveBeenCalledWith('templates/interfaces.hbs', {encoding: 'utf-8'});
+      expect(fs.writeFileSync).toHaveBeenCalledTimes(2);
+      expect(fs.writeFileSync.mock.calls[0][0]).toEqual('cwd/test/interfaces/test-module.interfaces.ts');
+      expect(fs.writeFileSync.mock.calls[1][0]).toEqual('cwd/test/interfaces/index.ts');
+    });
+  });
+  describe('scaffold', () => {
+    let context;
+    beforeEach(() => {
+      jest.spyOn(fs, 'writeFileSync').mockReturnValue(null);
+      jest.spyOn(fs, 'readFileSync');
+      jest.spyOn(fs, 'mkdirSync').mockReturnValue(null);
+      jest.spyOn(process, 'cwd').mockReturnValue('cwd');
+      context = {
+        camel: 'test',
+        capital: 'TEST',
+        kebab: 'test',
+        moduleName: 'TestModule',
+        name: 'test',
+        pascal: 'Test',
+        serviceName: 'TestService'
+      }
+    });
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+    test('create directories for package name and interfaces', () => {
+      scaffold(context, ['module']);
+      expect(fs.mkdirSync).toHaveBeenCalledTimes(2);
+      expect(fs.mkdirSync).toHaveBeenCalledWith('cwd/test');
+      expect(fs.mkdirSync).toHaveBeenCalledWith('cwd/test/interfaces');
+    });
+    test('create directories for package name and interfaces', () => {
+
+      scaffold(context, ['module']);
+      expect(fs.mkdirSync).toHaveBeenCalledTimes(2);
+      expect(fs.mkdirSync).toHaveBeenCalledWith('cwd/test');
+      expect(fs.mkdirSync).toHaveBeenCalledWith('cwd/test/interfaces');
+    });
+  });
+});
